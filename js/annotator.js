3fdd02dc463801862066f8226e849732b0f2c258
Tom Moor <tom.moor@gmail.com>
2013-09-07 21:33:28 -0700

    Dropbox saving

--- js/annotator.js
+++ js/annotator.js
@@ -1,47 +1,25 @@
 $(function(){
-
-    /*
     var img = new Image();
-    var canvas = $('<canvas/>').get(0);
-    canvas.width = $(window).width();
-    canvas.height = $(window).height();
+    var canvas = $('canvas').get(0);
+    
+    $('nav').animate({top: 0}, 500);
+    $('a.dropbox-save').click(function() {
+        var data = canvas.toDataURL('image/png');
+        console.log('saving modifications');
+        localStorage.lastCapture = data;
+        
+        chrome.runtime.sendMessage({save: true});
+    });
     
     img.onload = function() {
+        console.log(img.width, img.height);
+        
         var context = canvas.getContext('2d');
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);
-        canvas.toBlob(function(url){
-            console.log('blob response', url);
-            
-            $('body').prepend(Dropbox.createSaveButton(url, 'annotation.png'));
-            
-        }, 'image/png');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        $('canvas').sketch();
+        context.drawImage(img, 0, 0, img.width, img.height);
     };
-
-    img.src = localStorage.lastCapture;
-    */
     
-    $('body').prepend('<a class="dropbox-save">Save to Dropbox</a>');
-    $('a.dropbox-save').click(function() {
-        
-        var client = new Dropbox.Client({ key: "8yj4y7fh3x131iu" });
-        client.authDriver(new Dropbox.AuthDriver.Chrome({receiverPath: "chrome_oauth_receiver.html"}));
-        
-        console.log('authenticate');
-        client.authenticate(function(error, client) {
-          if (error) {
-              console.log(error);
-            // Replace with a call to your own error-handling code.
-            //
-            // Don't forget to return from the callback, so you don't execute the code
-            // that assumes everything went well.
-            return;
-          }
-
-          // Replace with a call to your own application code.
-          //
-          // The user authorized your app, and everything went well.
-          // client is a Dropbox.Client instance that you can use to make API calls.
-          console.log('authenticated', client);
-        });
-    });
+    img.src = localStorage.lastCapture;
 });
--- js/capture.js
+++ js/capture.js
@@ -3,10 +3,12 @@ function captureTab() {
         chrome.tabs.captureVisibleTab({format: 'png', quality: 1}, function(data){
             chrome.windows.getCurrent(function(win){
                 
-                console.log('saving and sending...', data);
-                localStorage.lastCapture = data;
+                console.log('saving and triggering...');
+                var filename = tab.url.toLowerCase().replace(/^https?/,'').replace(/[^\w ]+/g,'').replace(/ +/g,'-');
                 
-                chrome.tabs.sendMessage(tab.id, {width: win.width, height: win.height});
+                localStorage.lastCapture = data;
+                localStorage.filename = filename;
+                chrome.tabs.sendMessage(tab.id, {trigger: true});
             });
         });
     });
@@ -16,4 +18,58 @@ chrome.browserAction.onClicked.addListener(function(){
     console.log('clicked');
     
     captureTab();
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log('clicked save button');
+    
+    chrome.windows.getCurrent(function(win){
+    
+        var client = new Dropbox.Client({key: "8yj4y7fh3x131iu"});
+        client.authDriver(new Dropbox.AuthDriver.Chrome({receiverUrl: 'chrome_oauth_receiver.html'}));
+
+        client.authenticate({interactive: true}, function(error, client) {
+          if (error) {
+              console.log(error);
+            // Replace with a call to your own error-handling code.
+            //
+            // Don't forget to return from the callback, so you don't execute the code
+            // that assumes everything went well.
+            return;
+          }
+
+          // Replace with a call to your own application code.
+          //
+          // The user authorized your app, and everything went well.
+          // client is a Dropbox.Client instance that you can use to make API calls.
+          console.log('authenticated', client);
+          
+          var img = new Image();
+          var canvas = $('<canvas/>').get(0);
+          canvas.width = win.width;
+          canvas.height = win.height;
+
+          img.onload = function() {
+              console.log('converting...');
+              
+              var context = canvas.getContext('2d');
+              context.drawImage(img, 0, 0, win.width, win.height);
+              canvas.toData(function(data){
+                 
+                 console.log('saving...');
+                 
+                 // reader.result contains the contents of blob as a typed array
+                 client.writeFile(localStorage.filename + ".png",data, function(error, stat) {
+                   if (error) {
+                     return console.log(error);  // Something went wrong.
+                   }
+                   
+                   console.log("File saved as revision " + stat.revisionTag);
+                 });
+              }, 'image/png');
+          };
+          
+          img.src = localStorage.lastCapture;
+        });
+    });
 });
